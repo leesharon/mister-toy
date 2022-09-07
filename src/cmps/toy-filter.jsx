0f47e81cd0397c8ab45e23cd31148f897d781bf3
leesharon <leesharooon@gmail.com>
2022-09-07 21:08:37 +0300

    DONE filter

--- src/cmps/toy-filter.jsx
+++ src/cmps/toy-filter.jsx
@@ -1,7 +1,9 @@
 import { useFormRegister } from '../hooks/useFormRegister'
 import React from 'react'
-import { Formik } from 'formik'
-import { NativeSelect, Select } from '@mui/material'
+import { Field, Form, Formik } from 'formik'
+import { Button, NativeSelect, Select, TextField } from '@mui/material'
+import * as Yup from 'yup'
+import { Basic } from './my-form'
 
 export const ToyFilter = (props) => {
 
@@ -15,24 +17,24 @@ export const ToyFilter = (props) => {
     )
 
     return (
-        <React.Fragment>
-            <form className="toy-filter">
-                <section>
-                    <label htmlFor="txt">Name</label>
-                    <input {...register('txt', 'text')} />
-                </section>
-                <section>
-                    <label htmlFor="minPrice">Min Price</label>
-                    <input {...register('minPrice', 'number')} />
-                </section>
-                <NativeSelect
-                    {...register('inStock')}
-                >
-                    <option value="">All</option>
-                    <option value="in-stock">In Stock</option>
-                    <option value="out-of-stock">Out of Stock</option>
-                </NativeSelect>
-            </form>
-        </React.Fragment >
+        // <Basic />
+
+        <form className="toy-filter">
+            <section>
+                <label htmlFor="txt">Name</label>
+                <input {...register('txt', 'text')} />
+            </section>
+            <section>
+                <label htmlFor="minPrice">Min Price</label>
+                <input {...register('minPrice', 'number')} />
+            </section>
+            <NativeSelect
+                {...register('inStock')}
+            >
+                <option value="">All</option>
+                <option value="in-stock">In Stock</option>
+                <option value="out-of-stock">Out of Stock</option>
+            </NativeSelect>
+        </form>
     )
 }
